1178fa4872b194472d6f2d9c98237e3967eddff4
jeroanan <david.tosslehoff@gmail.com>
2016-08-03 23:54:09 +0100

    Tonight's work

---
+++ boinc-structs.rkt
@@ -0,0 +1,134 @@
+(struct get-state-result (host-info
+                          net-stats
+                          time-stats
+                          workunits
+                          results
+                          platform-name
+                          core-client-major-version
+                          core-client-minor-version
+                          core-client-release
+                          executing-as-daemon
+                          platform
+                          global-preferences))
+
+(struct host-info (timezone
+                   domain-name
+                   ip-addr
+                   host-cpid
+                   p-ncpus
+                   p-vendor
+                   p-model
+                   p-features
+                   p-fpops
+                   p-iops
+                   p-membw
+                   p-calculated
+                   p-vm-extensions-disabled
+                   m-nbytes
+                   m-cache
+                   m-swap
+                   d-total
+                   os-name
+                   os-version
+                   coprocs))
+
+(struct net-stats (bwup
+                   avg-up
+                   avg-time-up
+                   bwdown
+                   avg-down
+                   avg-time-down))
+
+(struct time-stats (on-frac
+                    connected-frac
+                    cpu-and-network-available-frac
+                    active-frac
+                    gpu-active-frac
+                    client-start-time
+                    total-start-time
+                    total-duration
+                    total-active-duration
+                    total-gpu-active-duration
+                    now
+                    previous-uptime
+                    session-active-duration
+                    session-gpu-active-duration))
+
+(struct global-preferences (source-project
+                            mod-time
+                            battery-charge-min-pct
+                            battery-max-temperature
+                            run-on-batteries
+                            run-if-user-active
+                            run-gpu-if-user-active
+                            suspend-if-no-recent-input
+                            suspend-cpu-usage
+                            start-hour
+                            end-hour
+                            net-start-hour
+                            net-end-hour
+                            leave-apps-in-memory
+                            confirm-before-connecting
+                            hangup-if-dialed
+                            dont-verify-images
+                            work-buf-min-days
+                            work-buf-additional-days
+                            max-ncpus-pct
+                            cpu-scheduling-period-minutes
+                            disk-interval
+                            disk-max-used-gb
+                            disk-max-used-pct
+                            disk-min-free-gb
+                            vm-max-used-pct
+                            ram-max-used-busy-pct
+                            ram-max-used-idle-pct
+                            idle-time-to-run
+                            max-bytes-sec-up
+                            max-bytes-sec-down
+                            cpu-usage-limit
+                            daily-xfer-limit-mb
+                            daily-xfer-period-days
+                            override-file-present
+                            network-wifi-only))
+
+(struct workunit (name
+                  app-name
+                  version-num
+                  rsc-fpops-est
+                  rsc-fpops-bound
+                  rsc-memory-bound
+                  rsc-disk-bound
+                  file-ref))
+
+(struct work-unit-file-ref (file-name
+                            open-name))
+
+(struct result (name
+                wu-name
+                version-num
+                plan-class
+                project-url
+                final-cpu-time
+                final-elapsed-time
+                exit-status
+                state
+                report-deadline
+                received-time
+                estimated-cpu-time-remaining
+                active-task))
+
+(struct active-task (active-task-state
+                     app-version-num
+                     slot
+                     pid
+                     scheduler-state
+                     checkpoint-cpu-time
+                     fraction-done
+                     current-cpu-time
+                     elapsed-time
+                     swap-size
+                     working-set-size
+                     working-set-size-smoothed
+                     page-fault-rate
+                     bytes-sent
+                     bytes-received))
--- boinc.rkt
+++ boinc.rkt
@@ -2,101 +2,11 @@
 
 (require racket/include)
 (include "boinc-xml.rkt")
+(include "boinc-structs.rkt")
 (include "xexpr-macros.rkt")
 
 (define (sublists-only element)
   (filter (lambda (x) (list? x)) (cddr element)))
-
-(struct get-state-result (host-info
-                          net-stats
-                          time-stats
-                          platform-name
-                          core-client-major-version
-                          core-client-minor-version
-                          core-client-release
-                          executing-as-daemon
-                          platform
-                          global-preferences))
-
-(struct host-info (timezone
-                   domain-name
-                   ip-addr
-                   host-cpid
-                   p-ncpus
-                   p-vendor
-                   p-model
-                   p-features
-                   p-fpops
-                   p-iops
-                   p-membw
-                   p-calculated
-                   p-vm-extensions-disabled
-                   m-nbytes
-                   m-cache
-                   m-swap
-                   d-total
-                   os-name
-                   os-version
-                   coprocs))
-
-(struct net-stats (bwup
-                   avg-up
-                   avg-time-up
-                   bwdown
-                   avg-down
-                   avg-time-down))
-
-(struct time-stats (on-frac
-                    connected-frac
-                    cpu-and-network-available-frac
-                    active-frac
-                    gpu-active-frac
-                    client-start-time
-                    total-start-time
-                    total-duration
-                    total-active-duration
-                    total-gpu-active-duration
-                    now
-                    previous-uptime
-                    session-active-duration
-                    session-gpu-active-duration))
-
-(struct global-preferences (source-project
-                            mod-time
-                            battery-charge-min-pct
-                            battery-max-temperature
-                            run-on-batteries
-                            run-if-user-active
-                            run-gpu-if-user-active
-                            suspend-if-no-recent-input
-                            suspend-cpu-usage
-                            start-hour
-                            end-hour
-                            net-start-hour
-                            net-end-hour
-                            leave-apps-in-memory
-                            confirm-before-connecting
-                            hangup-if-dialed
-                            dont-verify-images
-                            work-buf-min-days
-                            work-buf-additional-days
-                            max-ncpus-pct
-                            cpu-scheduling-period-minutes
-                            disk-interval
-                            disk-max-used-gb
-                            disk-max-used-pct
-                            disk-min-free-gb
-                            vm-max-used-pct
-                            ram-max-used-busy-pct
-                            ram-max-used-idle-pct
-                            idle-time-to-run
-                            max-bytes-sec-up
-                            max-bytes-sec-down
-                            cpu-usage-limit
-                            daily-xfer-limit-mb
-                            daily-xfer-period-days
-                            override-file-present
-                            network-wifi-only))
 
 (define (get-node stats stat-name)
   (if (eq? (first (first stats)) stat-name)
@@ -210,39 +120,92 @@
                           (gs 'override_file_present)
                           (gs 'network_wifi_only))))
 
+  (define (parse-results stats [output (list)])
+    (let*
+        ((this-result (sublists-only (first stats)))
+         (gs (lambda (x) (get-stat-value this-result x)))
+         (gse (lambda (x) (sublists-only (get-node this-result x))))
+         (r (result (gs 'name)
+                 (gs 'wu-name)
+                 (gs 'version_num)
+                 (gs 'plan_class)
+                 (gs 'project_url)
+                 (gs 'final_cpu_time)
+                 (gs 'final_elapsed_time)
+                 (gs 'exit_status)
+                 (gs 'state)
+                 (gs 'report_deadline)
+                 (gs 'received_time)
+                 (gs 'estimated_cpu_time_remaining)
+                 (parse-active-task (gse 'active_task))))
+         (new-output (append output (list r))))
+      (if (empty? (rest stats))
+          new-output
+          (parse-results (rest stats) new-output))))
+
+  (define (parse-active-task stats)
+    (let* ((gs (lambda (x) (get-stat-value stats  x))))
+      (active-task (gs 'active_task_state)
+                   (gs 'app_version_num)
+                   (gs 'slot)
+                   (gs 'pid)
+                   (gs 'scheduler_state)
+                   (gs 'checkpoint_cpu_time)
+                   (gs 'fraction_done)
+                   (gs 'current_cpu_time)
+                   (gs 'elapsed_time)
+                   (gs 'swap_size)
+                   (gs 'working_set_size)
+                   (gs 'working_set_size_smoothed)
+                   (gs 'page_fault_rate)
+                   (gs 'bytes_sent)
+                   (gs 'bytes_received))))
+
+  (define (parse-workunits stats [output (list)])
+    (let*
+        ((this-workunit (sublists-only (first stats)))
+         (gs (lambda (x) (get-stat-value this-workunit x)))
+         (gse (lambda (x) (sublists-only (get-node this-workunit x))))
+         (wu (workunit (gs 'name)
+                   (gs 'app_name)
+                   (gs 'version_num)
+                   (gs 'rsc_fpops_est)
+                   (gs 'rsc_fpops_bound)
+                   (gs 'rsc_memory_bound)
+                   (gs 'rsc_disk_bound)
+                   (parse-workunit-file-ref (gse 'file_ref))))
+         (new-output (append output (list wu))))
+    (if (empty? (rest stats))
+        new-output
+        (parse-workunits (rest stats) new-output))))
+
+  (define (parse-workunit-file-ref stats)
+    (let* ((gs (lambda (x) (get-stat-value stats  x))))
+      (work-unit-file-ref (gs 'file_name)
+                          (gs 'open_name))))
+
   (let* ((state-xml (xexpr-get-document-element (get-state-xml)))
          (client-state (cddr (get-node (cddr state-xml) 'client_state)))
          (gse (lambda (x) (sublists-only (get-node client-state x))))
          (gses (lambda (x) (third (get-node client-state x))))
-         (host-info (parse-host-info (gse 'host_info)))
-         (net-stats (parse-net-stats (gse 'net_stats)))
-         (time-stats (parse-time-stats (gse 'time_stats)))
-         (platform-name (gses 'platform_name))
-         (core-client-major-version (gses 'core_client_major_version))
-         (core-client-minor-version (gses 'core_client_minor_version))
-         (core-client-release (gses 'core_client_release))
-         (executing-as-daemon (gses 'executing_as_daemon))
-         (platform (gses 'platform))
-         (workunits (get-nodes client-state 'workunit))
-         (global-preferences (parse-global-preferences (gse 'global_preferences)))
-         (result (get-state-result
-                  host-info
-                  net-stats
-                  time-stats
-                  platform-name
-                  core-client-major-version
-                  core-client-minor-version
-                  core-client-release
-                  executing-as-daemon
-                  platform
-                  global-preferences)))
-    workunits))
+         (gns (lambda (x) (get-nodes client-state x))))
+    (get-state-result (parse-host-info (gse 'host_info))
+                      (parse-net-stats (gse 'net_stats))
+                      (parse-time-stats (gse 'time_stats))
+                      (parse-workunits (gns 'workunit))
+                      (parse-results (gns 'result))
+                      (gses 'platform_name)
+                      (gses 'core_client_major_version)
+                      (gses 'core_client_minor_version)
+                      (gses 'core_client_release)
+                      (gses 'executing_as_daemon)
+                      (gses 'platform)
+                      (parse-global-preferences (gse 'global_preferences)))))
 
 (define (get-host-info)
   (let* ((host-info-xml (xexpr-get-document-element (get-host-info-xml)))
          (host-info-node (sublists-only (cddr (get-node (cddr host-info-xml) 'host_info))))
-         (result (parse-host-info host-info-node)))
-    result))
+    (parse-host-info host-info-node)))
 
 (define (get-disk-usage)
   (let* ((disk-usage-xml (xexpr-get-document-element (get-disk-usage-xml))))
