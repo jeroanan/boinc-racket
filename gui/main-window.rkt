5da6bff8dc11f29486dc04409fda41973618bb3d
jeroanan <david.tosslehoff@gmail.com>
2016-09-09 22:51:53 +0100

    Refactor main-window.rkt
    Move functions to do with making tabs out of launch-gui.

--- gui/main-window.rkt
+++ gui/main-window.rkt
@@ -26,132 +26,167 @@
 (require "add-project-window.rkt")
 (require "widget-tools/button-tools.rkt")
 
+(define tab-height 300)
+(define tab-width 1000)
+
+;; The main window.
+(define frame (new frame%
+                   (label "BOINC-Racket")
+                   (height tab-height)
+                   (width tab-width)))
+
+(define (set-list-column-items list-ctrl column-no list-items [n 0])
+  ;; For the given list control and column number, set the contents.
+  (define (do-add)
+    (send list-ctrl set-string n (first list-items) column-no)
+    (set-list-column-items list-ctrl column-no (rest list-items) (+ n 1)))
+  
+  (if (empty? list-items) #t (do-add)))
+
+(define (add-list-column list-ctrl label width contents)
+  ;; Append a new column to a list box with the given label, width and contents
+  (define number-of-columns (length (send list-ctrl get-column-labels)))
+  (send list-ctrl append-column label)
+  (send list-ctrl set-column-width number-of-columns width 0 1000000)
+  (set-list-column-items list-ctrl number-of-columns contents))
+
+(define (new-list-box parent choices)
+  (new list-box%
+       [parent parent]
+       [choices choices]
+       [style (list 'single
+                    'variable-columns
+                    'column-headers
+                    'clickable-headers)]
+       [min-width tab-width]
+       [stretchable-width #t]
+       [stretchable-height #t]
+       [label #f]))
+
+(define (change-to-notices-panel)
+  ;; Draw the notices panel
+  (define notices-panel (new-panel "Notices"))
+  (draw-tab 0 (list notices-panel)))
+
+(define (change-to-projects-panel)
+  ;; Draw the projects panel and its child controls
+  (define projects (get-project-status))
+  
+  (define (gpf the-field)
+    ;; Get the given field from each of a list of project structs.
+    (map (lambda (x) (the-field x)) projects))
+  
+  (define project-names (gpf project-project-name))
+  (define project-accounts (gpf project-userid))
+  (define project-teams (gpf project-teamid))
+  (define work-done (gpf project-user-total-credit))
+  (define avg-work-done (gpf project-user-expavg-credit))
+  (define resource-share (gpf project-resource-share))
+  
+  (define (add-to-list label contents)
+    (add-list-column projects-list label 300 contents))
+  
+  (define projects-panel (new-panel "Attached Projects"))
+  (define hpane (new horizontal-pane%
+                     [parent projects-panel]))
+  
+  (define button-panel (new vertical-panel%
+                            [parent hpane]
+                            [alignment (list 'center 'top)]))
+  
+  (define button-maker (get-simple-button-maker button-panel))
+  
+  (define (do-nothing) #f)
+  (define update-button (button-maker "Update" do-nothing))
+  (define suspend-button (button-maker "Suspend" do-nothing))
+  (define no-new-tasks-button (button-maker "No new tasks" do-nothing))
+  (define detach-project (button-maker "Detach" do-nothing))
+  
+  (define projects-list (new-list-box hpane project-names))
+  (send projects-list set-column-width 0 300 0 1000000)
+  (send projects-list set-column-label 0 "Project")
+  
+  (add-to-list "Account" project-accounts)
+  (add-to-list "Team" project-teams)
+  (add-to-list "Work done" work-done)
+  (add-to-list "Avg. work done" avg-work-done)
+  (add-to-list "Resource share" resource-share)    
+  
+  (draw-tab 1 (list projects-panel)))
+
+(define (change-to-tasks-panel)
+  
+  (define results (get-results))
+  
+  (define (gtf the-field)
+    (map (lambda (x) (the-field x)) results))
+  
+  (define task-names (gtf result-name))
+  (define project-urls (gtf result-project-url))
+  (define report-deadlines (gtf result-report-deadline))
+  (define task-states (gtf result-state))
+  
+  (define (get-fraction-done)
+    (map (lambda (x) (active-task-fraction-done (result-active-tasks x))) results))
+  
+  (define tasks-panel (new-panel "Tasks"))
+  
+  (define tasks-list (new-list-box tasks-panel project-urls))
+  (send tasks-list set-column-width 0 300 0 1000000)
+  (send tasks-list set-column-label 0 "Project")
+
+  (define (add-to-list label contents)
+    (add-list-column tasks-list label 300 contents))
+
+  (add-to-list "% Done" (get-fraction-done))
+  (add-to-list "Deadline" report-deadlines)
+  (add-to-list "Status" task-states)    
+  (add-to-list "Name" task-names)
+
+  (draw-tab 2 (list tasks-panel)))
+
+(define (change-to-transfers-panel)
+  (define transfers-panel (new-panel "Transfers"))
+  (draw-tab 3 (list transfers-panel)))
+
+(define (change-to-statistics-panel)
+  (define statistics-panel (new-panel "Statistics"))
+  (draw-tab 4 (list statistics-panel)))
+
+(define (change-to-disk-panel)
+  (define disk-panel (new-panel "Disk"))
+  (draw-tab 5 (list disk-panel)))  
+
+(define (get-tab-panel)
+  ;; Initialise the tab strip that runs along the top of the main window.
+  (new tab-panel%
+       [parent frame]
+       [choices (list "Notices"
+                      "Projects"
+                      "Tasks"
+                      "Transfers"
+                      "Statistics"
+                      "Disk")]
+       [callback (lambda (tp e)
+                   (case (send tp get-selection)
+                     ((0) (change-to-notices-panel))
+                     ((1) (change-to-projects-panel))
+                     ((2) (change-to-tasks-panel))
+                     ((3) (change-to-transfers-panel))
+                     ((4) (change-to-statistics-panel))
+                     ((5) (change-to-disk-panel))))]))
+
+(define tab-panel (get-tab-panel))
+
+(define (new-panel label)  
+  (new panel% [parent tab-panel]))
+
+(define (draw-tab panel-index panel-controls)
+  ;; Tells tab-panel to show the given panel with the given controls.
+  (send tab-panel set-selection panel-index)
+  (send tab-panel change-children (lambda (c) panel-controls)))
+
 (define (launch-gui)
-  (define tab-height 300)
-  (define tab-width 1000)
-
-  (define frame (new frame%
-                     (label "BOINC-Racket")
-                     (height tab-height)
-                     (width tab-width)))
-
-  (define tab-panel (new tab-panel%
-                         (parent frame)
-                         (choices (list "Notices"
-                                        "Projects"
-                                        "Tasks"
-                                        "Transfers"
-                                        "Statistics"
-                                        "Disk"))
-                         (callback (lambda (tp e)
-                                     (case (send tp get-selection)
-                                       ((0) (change-to-notices-panel))
-                                       ((1) (change-to-projects-panel))
-                                       ((2) (change-to-tasks-panel))
-                                       ((3) (change-to-transfers-panel))
-                                       ((4) (change-to-statistics-panel))
-                                       ((5) (change-to-disk-panel)))))))
-
-  (define (new-panel label)
-    (new panel%
-         (parent tab-panel)))
-
-  (define (draw-tab panel-index panel-controls)
-    (send tab-panel set-selection panel-index)
-    (send tab-panel change-children (lambda (c) panel-controls)))
-  
-  (define (change-to-notices-panel)
-
-    (define notices-panel (new-panel "Notices"))
-    (draw-tab 0 (list notices-panel)))
-
-  (define (change-to-projects-panel)
-
-    (define projects (get-project-status))
-      
-    (define (gpf the-field)
-      (map (lambda (x) (the-field x)) projects))
-
-    (define project-names (gpf project-project-name))
-    (define project-accounts (gpf project-userid))
-    (define project-teams (gpf project-teamid))
-    (define work-done (gpf project-user-total-credit))
-    (define avg-work-done (gpf project-user-expavg-credit))
-    (define resource-share (gpf project-resource-share))
-    
-    (define (add-to-list label contents)
-      (add-list-column projects-list label 300 contents))
-
-    (define projects-panel (new-panel "Attached Projects"))
-    (define hpane (new horizontal-pane%
-                            [parent projects-panel]))
-
-    (define button-panel (new vertical-panel%
-                              [parent hpane]
-                              [alignment (list 'center 'top)]))
-
-    (define button-maker (get-simple-button-maker button-panel))
-
-    (define (do-nothing) #f)
-    (define update-button (button-maker "Update" do-nothing))
-    (define suspend-button (button-maker "Suspend" do-nothing))
-    (define no-new-tasks-button (button-maker "No new tasks" do-nothing))
-    (define detach-project (button-maker "Detach" do-nothing))
-
-    (define projects-list (new-list-box hpane project-names))
-    (send projects-list set-column-width 0 300 0 1000000)
-    (send projects-list set-column-label 0 "Project")
-    
-    (add-to-list "Account" project-accounts)
-    (add-to-list "Team" project-teams)
-    (add-to-list "Work done" work-done)
-    (add-to-list "Avg. work done" avg-work-done)
-    (add-to-list "Resource share" resource-share)    
-
-    (draw-tab 1 (list projects-panel)))
-  
-  (define (change-to-tasks-panel)
-    
-    (define results (get-results))
-    
-    (define (gtf the-field)
-      (map (lambda (x) (the-field x)) results))
-    
-    (define task-names (gtf result-name))
-    (define project-urls (gtf result-project-url))
-    (define report-deadlines (gtf result-report-deadline))
-    (define task-states (gtf result-state))
-
-    (define (get-fraction-done)
-      (map (lambda (x) (active-task-fraction-done (result-active-tasks x))) results))
- 
-    (define tasks-panel (new-panel "Tasks"))
-
-    (define tasks-list (new-list-box tasks-panel project-urls))
-    (send tasks-list set-column-width 0 300 0 1000000)
-    (send tasks-list set-column-label 0 "Project")
-
-    (define (add-to-list label contents)
-      (add-list-column tasks-list label 300 contents))
-    
-    (add-to-list "% Done" (get-fraction-done))
-    (add-to-list "Deadline" report-deadlines)
-    (add-to-list "Status" task-states)    
-    (add-to-list "Name" task-names)
-
-    (draw-tab 2 (list tasks-panel)))
-  
-  (define (change-to-transfers-panel)
-    (define transfers-panel (new-panel "Transfers"))
-    (draw-tab 3 (list transfers-panel)))
-
-  (define (change-to-statistics-panel)
-    (define statistics-panel (new-panel "Statistics"))
-    (draw-tab 4 (list statistics-panel)))
-
-  (define (change-to-disk-panel)
-    (define disk-panel (new-panel "Disk"))
-    (draw-tab 5 (list disk-panel)))  
 
   (define menu-bar (new menu-bar%
                         (parent frame)))
@@ -208,36 +243,6 @@
                                    "Event log..."
                                    "Event log diagnostic flags..."))
   (make-dud-menu advanced-menu advanced-menu-items)
-                                   
-  ;;(define help-menu (new-menu "&Help"))  
-
-  (define (new-list-box parent choices)
-    (new list-box%
-           [parent parent]
-           [choices choices]
-           [style (list 'single
-                        'variable-columns
-                        'column-headers
-                        'clickable-headers)]
-           [min-width tab-width]
-           [stretchable-width #t]
-           [stretchable-height #t]
-           [label #f]))
-  
-  (define (set-list-column-items list-ctrl column-no list-items [n 0])
-    (define (do-add)
-      (send list-ctrl set-string n (first list-items) column-no)
-      (set-list-column-items list-ctrl column-no (rest list-items) (+ n 1)))
-    
-    (if (empty? list-items)
-        #t
-        (do-add)))
-
-  (define (add-list-column list-ctrl label width contents)
-    (define number-of-columns (length (send list-ctrl get-column-labels)))
-    (send list-ctrl append-column label)
-    (send list-ctrl set-column-width number-of-columns width 0 1000000)
-    (set-list-column-items list-ctrl number-of-columns contents))
 
   (send frame show #t)
   (send tab-panel set-selection 1)
