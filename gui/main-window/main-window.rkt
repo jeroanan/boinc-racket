826bd6f43e3c9e9d0b55f262c7c144d3318ece6f
jeroanan <david.tosslehoff@gmail.com>
2016-09-13 21:22:14 +0100

    Fix bug that caused error with menu if main window
    was closed and then opened again.

--- gui/main-window/main-window.rkt
+++ gui/main-window/main-window.rkt
@@ -74,20 +74,19 @@
 (define (change-to-statistics-panel) (change-to-panel 4))
 (define (change-to-disk-panel) (change-to-panel 5))
 
-(define (launch-gui)
-
+(define (init-menu)
   (define menu-bar (new menu-bar%
                         (parent frame)))
-
+  
   (define (new-menu label)
     (new menu% (parent menu-bar) (label label)))
   
   (define (new-menu-item label parent callback)
     (new menu-item% [label label] [parent parent] [callback callback]))
-
+  
   (define (dud-menu-item label parent)
     (new-menu-item label parent (lambda (x y) #f)))
-
+  
   (define file-menu (new-menu "&File"))
   (new-menu-item "&Close Window" file-menu (lambda (x y) (send frame show #f)))
   (dud-menu-item "Exit" file-menu)
@@ -97,12 +96,12 @@
       (dud-menu-item label parent-menu)
       (make-dud-menu parent-menu (rest items)))
     (aif (not (empty? items)) (add-menu-item (first items))))
-
+  
   (define view-menu (new-menu "&View"))
-
+  
   (define (view-menu-item caption f)
     (new-menu-item caption view-menu (lambda (x y) (f))))
-
+  
   (view-menu-item "&Notices" change-to-notices-panel)
   (view-menu-item "&Projects" (lambda () (change-to-projects-panel tab-panel)))
   (view-menu-item "&Tasks" (lambda () (change-to-tasks-panel tab-panel)))
@@ -111,26 +110,27 @@
   (view-menu-item "&Disk usage" change-to-disk-panel)
   
   (define tools-menu (new-menu "&Tools"))
-  (new-menu-item "&Add project or account manager..." tools-menu (lambda (x y)
-                                                                   (show-add-project-window
-                                                                    frame)))
-
+  (new-menu-item "&Add project or account manager..." tools-menu
+                 (lambda (x y) (show-add-project-window frame)))
+  
   (define tools-menu-items (list "&Options..."
                                  "Computing &preferences..."))
   (make-dud-menu tools-menu tools-menu-items)
-
+  
   ;;(define activity-menu (new-menu "&Activity"))
   (define advanced-menu (new-menu "A&dvanced"))
   (define advanced-menu-items (list "Select computer..."
-                                   "Shut down connect client..."
-                                   "Run CPU benchmarks"
-                                   "Do network communication"
-                                   "Read config files"
-                                   "Read local prefs file"
-                                   "Event log..."
-                                   "Event log diagnostic flags..."))
-  (make-dud-menu advanced-menu advanced-menu-items)
+                                    "Shut down connect client..."
+                                    "Run CPU benchmarks"
+                                    "Do network communication"
+                                    "Read config files"
+                                    "Read local prefs file"
+                                    "Event log..."
+                                    "Event log diagnostic flags..."))
+  (make-dud-menu advanced-menu advanced-menu-items))
 
+(define (launch-gui)
+  (aif (not (send frame get-menu-bar)) (init-menu))
   (send frame show #t)
   (send tab-panel set-selection 1)
   (change-to-tasks-panel tab-panel))
