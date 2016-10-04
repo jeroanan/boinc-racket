c15227235eabced160844e39b0112421c4b56602
jeroanan <david.tosslehoff@gmail.com>
2016-10-04 21:42:54 +0100

    Add attach to account manager UI, but it doesn't do anything yet.

--- gui/add-project-window.rkt
+++ gui/add-project-window.rkt
@@ -24,7 +24,8 @@
          "../macros.rkt")
 (require "attach-project-window.rkt")
 (require "widget-tools/button-tools.rkt"
-         "widget-tools/list-tools.rkt")
+         "widget-tools/list-tools.rkt"
+	 "widget-tools/tab-tools.rkt")
 
 (provide show-add-project-window)
 
@@ -39,36 +40,97 @@
                      [min-width min-width]
                      [min-height min-height]))
 
-  (define available-projects
-    (sort (get-all-projects-list)
-          (lambda (x y) (string<? (available-project-name x)
-                                  (available-project-name y)))))
+  (define (do-nothing) #f)
 
-  (define available-project-names (map (lambda (x) (available-project-name x))
-                                       available-projects))
+  (define tab-panel
+    (new tab-panel%
+         [parent dialog]
+	 [choices (list "Project"
+	                "Account Manager")]
+	 [callback (lambda (tp e)
+	             (case (send tp get-selection)
+		       ((0) (change-to-projects-panel))
+		       ((1) (change-to-account-manager-panel))))]))
 
-  (define projects-list (new-list-box dialog min-width available-project-names))
-  (send projects-list min-height min-height)
+  (define dt ((curry draw-tab) tab-panel))
 
-  (define (next-button-click)
-    (define selected-data (get-listbox-selected-data projects-list))
-    (aif (available-project? selected-data)
-        (show-attach-project-window dialog (available-project-url selected-data))))
+  (define (change-to-projects-panel)
+    (define available-projects
+      (sort (get-all-projects-list)
+            (lambda (x y) (string<? (available-project-name x)
+                                    (available-project-name y)))))
+  
+    (define available-project-names (map (lambda (x) (available-project-name x))
+                                         available-projects))
+  
+    (define projects-panel
+      (new panel%
+        [parent tab-panel]))
+  
+    (define vpanel
+      (new vertical-panel%
+        [parent projects-panel]))
 
-  (define button-container (new horizontal-pane%
-                                [parent dialog]
+    (define projects-list (new-list-box vpanel min-width available-project-names))
+    (send projects-list min-height min-height)
+  
+    (define (next-button-click)
+      (define selected-data (get-listbox-selected-data projects-list))
+      (aif (available-project? selected-data)
+          (show-attach-project-window dialog (available-project-url selected-data))))
+  
+    (define button-container (new horizontal-pane%
+                                  [parent vpanel]
+                                  [alignment (list 'right 'center)]))
+  
+    (define button-maker (get-simple-button-maker button-container))
+    (define next-button (button-maker "&Next" next-button-click))
+    (define cancel-button (button-maker "&Cancel"
+                                        (lambda () (send dialog show #f))))
+  
+    (set-listbox-data projects-list available-projects)
+  
+    (send projects-list set-column-label 0 "Name")
+    (send projects-list set-selection 0)
+    (send projects-list focus)
+    (draw-tab tab-panel 0 (list projects-panel)))
+  
+  (define (change-to-account-manager-panel)
+    (define account-manager-panel
+      (new panel%
+        [parent tab-panel]))
+    
+    (define vpanel
+      (new vertical-panel%
+        [parent account-manager-panel]))
+	
+    (define account-manager-url
+      (new text-field%
+        [label "URL"]
+	[parent vpanel]))
+
+    (define username
+      (new text-field%
+        [label "Username"]
+	[parent vpanel]))
+
+    (define password
+      (new text-field%
+        [label "Password"]
+	[parent vpanel]
+	[style (list 'single 'password)]))
+
+    (define button-container (new horizontal-pane%
+                                [parent vpanel]
                                 [alignment (list 'right 'center)]))
 
-  (define button-maker (get-simple-button-maker button-container))
-  (define next-button (button-maker "&Next" next-button-click))
-  (define cancel-button (button-maker "&Cancel"
-                                      (lambda () (send dialog show #f))))
-
-  (set-listbox-data projects-list available-projects)
-
-  (send projects-list set-column-label 0 "Name")
-  (send projects-list set-selection 0)
-  
-  (send dialog show #t)
-  (send projects-list focus))
+    (define button-maker (get-simple-button-maker button-container))
+    (draw-tab tab-panel 1 (list account-manager-panel))
+    
+    (define cancel-button (button-maker "&Cancel" (lambda () (send dialog show #f))))
+    (define ok-button (button-maker "&OK" do-nothing))
+    (draw-tab tab-panel 1 (list account-manager-panel)))
+    
+  (change-to-projects-panel)
+  (send dialog show #t))
                      
